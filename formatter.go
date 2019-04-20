b451c156f0a82e10b92e017f3bd59f9834d969fb
Evgeniy Bastrykov <vporoshok@gmail.com>
2019-04-20 20:31:00 +0400

    feat(helper): formatter to wrap, quote and prefix strings

---
+++ formatter.go
@@ -0,0 +1,149 @@
+package pogo
+
+import (
+	"bytes"
+	"io"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+// Formatter to write text with an options
+type Formatter struct {
+	// Border is an prefix to each line
+	Border string
+	// Prefix is an extra prefix to first line between border and text
+	Prefix string
+	// Width of line to wrap (no wrap if less or equal zero)
+	Width int
+
+	buffer *bytes.Buffer
+	output io.Writer
+}
+
+// NewFormatter to write in w
+func NewFormatter(w io.Writer) *Formatter {
+
+	return &Formatter{
+		buffer: &bytes.Buffer{},
+		output: w,
+	}
+}
+
+// Format text
+func (f *Formatter) Format(text string) (err error) {
+	defer func() {
+		if rec := recover(); rec != nil {
+			type withStacktrace interface {
+				error
+				StackTrace() errors.StackTrace
+			}
+			if e, ok := rec.(withStacktrace); ok {
+				err = e
+			} else {
+				panic(rec)
+			}
+		}
+	}()
+
+	f.write(text)
+
+	return nil
+}
+
+func (f *Formatter) write(text string) {
+	lines := f.splitLines(text)
+	if f.Prefix != "" {
+		f.mustWrite(f.output, f.Border)
+		f.mustWrite(f.output, f.Prefix)
+		if f.isOneQuotedLine(lines) {
+			f.mustWrite(f.output, "\"")
+			f.mustWrite(f.output, lines[0])
+			f.mustWrite(f.output, "\"\n")
+
+			return
+		}
+		f.mustWrite(f.output, "\"\"\n")
+	}
+
+	for _, line := range lines {
+		f.writeLine(line)
+	}
+}
+
+func (f *Formatter) isOneQuotedLine(lines []string) bool {
+	if len(lines) != 1 {
+
+		return false
+	}
+	if f.Width < 1 {
+
+		return true
+	}
+
+	return len(f.Border)+len(f.Prefix)+2+len(lines[0]) <= f.Width
+}
+
+func (f *Formatter) splitLines(text string) []string {
+	if f.Prefix == "" {
+
+		return strings.Split(text, "\n")
+	}
+
+	return strings.SplitAfter(f.escape(text), "\\n")
+}
+
+func (f *Formatter) escape(text string) string {
+	return strings.NewReplacer(
+		"\"", "\\\"",
+		"\\", "\\\\",
+		"\n", "\\n",
+		"\r", "\\r",
+		"\t", "\\t",
+	).Replace(text)
+}
+
+func (f *Formatter) len() int {
+	n := f.buffer.Len() + len(f.Border)
+	if f.Prefix != "" {
+		n += 2
+	}
+
+	return n
+}
+
+func (f *Formatter) writeLine(line string) {
+	if f.Width > 0 {
+		for _, word := range strings.SplitAfter(line, " ") {
+			if f.len()+len(word) > f.Width {
+				f.flush()
+			}
+			f.mustWrite(f.buffer, word)
+		}
+	} else {
+		f.mustWrite(f.buffer, line)
+	}
+	f.flush()
+}
+
+func (f *Formatter) flush() {
+	if f.buffer.Len() == 0 && f.Width > 0 {
+		return
+	}
+	f.mustWrite(f.output, f.Border)
+	if f.Prefix != "" {
+		f.mustWrite(f.output, `"`)
+	}
+	f.mustWrite(f.output, f.buffer.String())
+	if f.Prefix != "" {
+		f.mustWrite(f.output, `"`)
+	}
+	f.mustWrite(f.output, "\n")
+	f.buffer.Reset()
+}
+
+func (Formatter) mustWrite(w io.Writer, s string) {
+	if _, err := io.WriteString(w, s); err != nil {
+		panic(errors.WithStack(err))
+	}
+}
---
+++ formatter_test.go
@@ -0,0 +1,149 @@
+package pogo_test
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/vporoshok/pogo"
+)
+
+func TestFormatter(t *testing.T) {
+	join := func(lines ...string) string {
+
+		return strings.Join(lines, "\n")
+	}
+
+	cases := [...]struct {
+		name   string
+		text   string
+		border string
+		prefix string
+		width  int
+		result string
+	}{
+		{
+			name: "msgid",
+			text: join(
+				"Some long text with break lines and",
+				"veryveryveryveryveryveryverylong word",
+			),
+			border: "",
+			prefix: "msgid ",
+			width:  30,
+			result: join(
+				`msgid ""`,
+				`"Some long text with break "`,
+				`"lines and\n"`,
+				`"veryveryveryveryveryveryverylong "`,
+				`"word"`,
+				``,
+			),
+		},
+		{
+			name: "msgid",
+			text: join(
+				`Some long text "with break" lines and`,
+				``,
+				``,
+				`veryveryveryveryveryveryverylong word`,
+			),
+			border: "",
+			prefix: "msgid ",
+			width:  30,
+			result: join(
+				`msgid ""`,
+				`"Some long text \"with "`,
+				`"break\" lines and\n"`,
+				`"\n"`,
+				`"\n"`,
+				`"veryveryveryveryveryveryverylong "`,
+				`"word"`,
+				``,
+			),
+		},
+		{
+			name: "previous msgid",
+			text: join(
+				"Some long text with break lines and",
+				"veryveryveryveryveryveryverylong word",
+			),
+			border: "#| ",
+			prefix: "msgid ",
+			width:  30,
+			result: join(
+				`#| msgid ""`,
+				`#| "Some long text with "`,
+				`#| "break lines and\n"`,
+				`#| "veryveryveryveryveryveryverylong "`,
+				`#| "word"`,
+				``,
+			),
+		},
+		{
+			name: "transaltor comment",
+			text: join(
+				"Some long text with break lines and",
+				"veryveryveryveryveryveryverylong word",
+			),
+			border: "# ",
+			prefix: "",
+			width:  0,
+			result: join(
+				`# Some long text with break lines and`,
+				`# veryveryveryveryveryveryverylong word`,
+				``,
+			),
+		},
+		{
+			name: "prefix oneline",
+			text: join(
+				"Some short text",
+			),
+			border: "",
+			prefix: "msgid ",
+			width:  0,
+			result: join(
+				`msgid "Some short text"`,
+				``,
+			),
+		},
+	}
+
+	result := &bytes.Buffer{}
+	f := pogo.NewFormatter(result)
+	for _, c := range cases {
+		// nolint:scopelint
+		t.Run(c.name, func(t *testing.T) {
+			f.Border = c.border
+			f.Prefix = c.prefix
+			f.Width = c.width
+			result.Reset()
+			require.NoError(t, f.Format(c.text))
+			assert.Equal(t, c.result, result.String())
+		})
+	}
+}
+
+type brokenWriter struct{}
+
+func (brokenWriter) Write(_ []byte) (int, error) {
+
+	return 0, errors.New("error")
+}
+
+func TestFormatterError(t *testing.T) {
+	f := pogo.NewFormatter(brokenWriter{})
+	assert.EqualError(t, f.Format("test"), "error")
+}
+
+func TestFormatterPanic(t *testing.T) {
+	f := pogo.NewFormatter(nil)
+	assert.Panics(t, func() {
+		_ = f.Format("test")
+	})
+}
